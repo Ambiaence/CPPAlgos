564032327f5337cb15071887695053668bb1f4d4
FocusUser <georgekentallen@gmail.com>
2022-09-08 08:42:33 -0700

    Started DayOfProgrammers

---
+++ DayOfTheProgrammer/main.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+/*
+Marie invented a Time Machine and wants to test it by time-traveling to visit Russia on the Day of the Programmer (the 256th day of the year) during a year in the inclusive range from 1700 to 2700.
+
+From 1700 to 1917, Russia's official calendar was the Julian calendar; since 1919 they used the Gregorian calendar system. The transition from the Julian to Gregorian calendar system occurred in 1918, when the next day after January 31st was February 14th. This means that in 1918, February 14th was the 32nd day of the year in Russia.
+
+January: 31st -> Feb 14th on 1918
+
+
+In both calendar systems, February is the only month with a variable amount of days; it has 29 days during a leap year, and 28 days during all other years. In the Julian calendar, leap years are divisible by 4; in the Gregorian calendar, leap years are either of the following:
+
+    Divisible by 400.
+    Divisible by 4 and not divisible by 100.
+
+Given a year,
+, find the date of the 256th day of that year according to the official Russian calendar during that year. Then print it in the format dd.mm.yyyy, where dd is the two-digit day, mm is the two-digit month, and yyyy is .
+
+ * Complete the 'dayOfProgrammer' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER year as parameter.
+ */
+
+string dayOfProgrammer(int year) {
+	string str = "yea";
+	if (year < 1918) {	 //Before 
+		if (year%4) {
+			str = str + "8.12." + to_string(year);
+		} else {
+				
+		}
+	} else if ( year > 1918 ) { //After 
+	} else {  //On
+	}
+	cout << str << "\n";
+	return str;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string year_temp;
+    getline(cin, year_temp);
+
+    int year = stoi(ltrim(rtrim(year_temp)));
+
+    string result = dayOfProgrammer(year);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
